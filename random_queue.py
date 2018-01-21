8f222225afbdab08e5ee907b9a8a259b707287a0
nhv.vin <nhv.vin@gmail.com>
2018-01-22 01:27:25 +0700

    fix new code

---
+++ random_queue.py
@@ -0,0 +1,63 @@
+import numpy as np
+
+
+class RandomQueue(object):
+    def __init__(self, weight_list):
+        self.list_messages = self.init_list_messages(weight_list)
+        self.weight_list = weight_list
+        self.ratio_weights = self.weighted(self.weight_list)
+        self.acc_ratio_weights = [0] + list(np.cumsum(self.ratio_weights))
+
+    def init_list_messages(self, probabilities_message):
+        """
+        Initialize list base one weight message list.
+        It will return integer list base on index of weight message list.
+        """
+        if 1 <= len(probabilities_message) <= 1000:
+            _list = list()
+            for i in range(len(probabilities_message)):
+                _list.append(i + 1)
+            return _list
+        else:
+            raise ValueError('Value mus between 1 to 1000')
+
+    def weighted(self, probabilities_message):
+        """
+        Calculate weight of message. Weight of message / Sum(Weight of message).
+        """
+        _sum_messages = sum(probabilities_message)
+        if 1 <= len(probabilities_message) <= 1000:
+            _weighted = []
+            for idx, x in enumerate(probabilities_message):
+                if isinstance(x, int):
+                    _weighted.append(x / _sum_messages)
+                else:
+                    raise TypeError('Weight must be Integer')
+            return _weighted
+        else:
+            raise ValueError('Value must between 1 to 1000')
+
+    @staticmethod
+    def find_interval(x, partition):
+        """
+        Find_interval -> i, "i" will be the smallest index
+        for which applies x < partition[i]. If no such index exists
+        "i" will be set to len(partition)
+        """
+        for i in range(0, len(partition)):
+            if x < partition[i]:
+                return i
+        return len(partition)
+
+    def next(self):
+        """
+        """
+        x = np.random.random()
+        index = self.find_interval(x, self.acc_ratio_weights)
+        _random = self.list_messages[index - 1]
+        return _random
+
+
+if __name__ == "__main__":
+    random_queue = RandomQueue([50, 30, 60])
+    print(random_queue.next())
--- test_message_random.py
+++ test_message_random.py
@@ -1,53 +1,51 @@
 import unittest
-import message_random
 from collections import Counter
 import math
 import operator
+import random_queue
 
 
 class MessageRandomTest(unittest.TestCase):
     def setUp(self):
-        self.messageRandom = message_random.MessageRandom()
+        self.message_random = random_queue.RandomQueue([50, 30, 60])
 
     def test_length_init_func(self):
-        self.assertEqual(len(self.messageRandom.init_list([10, 20, 30])), 3)
+        self.assertEqual(len(self.message_random.list_messages), 3)
 
     def test_init_func_only_accept_from_0_to_1000(self):
         with self.assertRaises(ValueError):
-            self.messageRandom.init_list([])
+            random_queue.RandomQueue([])
 
         with self.assertRaises(ValueError):
-            self.messageRandom.init_list([1] * 1001)
+            random_queue.RandomQueue([1] * 1001)
 
     def test_weighted_calculatetion(self):
-        self.assertEqual(self.messageRandom.weighted([20, 10, 30]), [0.3333333333333333, 0.16666666666666666, 0.5])
+        self.assertEqual(random_queue.RandomQueue([20, 10, 30]).ratio_weights, [0.3333333333333333, 0.16666666666666666, 0.5])
 
     def test_weighted_func_only_accept_from_0_to_1000(self):
         with self.assertRaises(ValueError):
-            self.messageRandom.weighted([])
+            _ratio_weights = random_queue.RandomQueue([]).ratio_weights
 
         with self.assertRaises(ValueError):
-            self.messageRandom.weighted([1] * 1001)
+            _ratio_weights = random_queue.RandomQueue([1] * 1001).ratio_weights
 
     def test_weighted_func_only_accept_integer_value(self):
         with self.assertRaises(TypeError):
-            self.messageRandom.weighted([0.1, 20, 30])
+            _list_messages = random_queue.RandomQueue([0.1, 20, 30]).list_messages
 
     def test_probability_output_should_around_probability_input(self):
-        _message = [50, 30, 60]
-        _faces_of_die = self.messageRandom.init_list(_message)
-        _weights = self.messageRandom.weighted(_message)
+        _random_queue = self.message_random
         _outcomes = []
         n = 10000
         for _ in range(n):
-            _outcomes.append(self.messageRandom.run_random(_faces_of_die, _weights))
+            _outcomes.append(_random_queue.next())
             c = Counter(_outcomes)
 
         for key in c:
             c[key] = c[key] / n
 
         probabiliti_outcome = dict(c)
-        for index, i in enumerate(_weights):
+        for index, i in enumerate(_random_queue.ratio_weights):
             if operator.eq(i, 0):
                 self.assertIsNone(probabiliti_outcome.get(index + 1))
             else:
